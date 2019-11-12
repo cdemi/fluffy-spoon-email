70eb0829181fd44060193e7311fb8af963abb50a
Shaun Tabone <shaun.tabone@gig.com>
2019-11-12 09:27:52 +0100

    Cleanup and updated README

--- src/fluffyspoon.email/Options/SmtpOptions.cs
+++ src/fluffyspoon.email/Options/SmtpOptions.cs
@@ -3,8 +3,11 @@
     public class SmtpOptions
     {
         public const string DefaultSectionName = "Smtp";
+        
         public string Hostname { get; set; }
+
         public string Username { get; set; }
+        
         public string Password { get; set; }
     }
 }
